1889be9ad18a793796c87b552d8505d860e57dfc
Eugen Ciur <eugen@papermerge.com>
2022-12-26 14:30:50 +0100

    db level contraints

--- core/models.py
+++ core/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 
 
+FOLDER_TYPE = 'folder'
+DOCUMENT_TYPE = 'document'
+
+NODE_TYPES = (
+    (FOLDER_TYPE, 'folder'),
+    (DOCUMENT_TYPE, 'document')
+)
+
+
 class Node(models.Model):
-
-    FOLDER_TYPE = 'folder'
-    DOCUMENT_TYPE = 'document'
-
-    NODE_TYPES = (
-        (FOLDER_TYPE, 'folder'),
-        (DOCUMENT_TYPE, 'document')
-    )
 
     parent = models.ForeignKey(
         "self",
@@ -20,5 +21,18 @@ class Node(models.Model):
     type = models.CharField(
         max_length=8,
         choices=NODE_TYPES,
-        default=DOCUMENT_TYPE
+        default=DOCUMENT_TYPE,
+        null=False,
+        blank=False
     )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['parent', 'title'], name='unique title per parent'
+            ),
+            models.CheckConstraint(
+                name="Node can be either document or folder only",
+                check=(models.Q(type__in=(FOLDER_TYPE, DOCUMENT_TYPE)))
+            )
+        ]
