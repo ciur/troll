32ebc405b7d21aaefcd89deb1602dbb6f584992b
Eugen Ciur <eugen@papermerge.com>
2022-12-26 14:22:38 +0100

    add migrations

--- core/models.py
+++ core/models.py
@@ -1,5 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
 class Node(models.Model):
-    
+
+    FOLDER_TYPE = 'folder'
+    DOCUMENT_TYPE = 'document'
+
+    NODE_TYPES = (
+        (FOLDER_TYPE, 'folder'),
+        (DOCUMENT_TYPE, 'document')
+    )
+
+    parent = models.ForeignKey(
+        "self",
+        on_delete=models.CASCADE
+    )
+
+    title = models.CharField(max_length=256)
+    type = models.CharField(
+        max_length=8,
+        choices=NODE_TYPES,
+        default=DOCUMENT_TYPE
+    )
